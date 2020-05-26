9cee5285cb38f9dedbbaefe1503e9a479fa6ab62
Oscar vallin <arnoldvallin@gmail.com>
2020-05-25 21:59:29 -0500

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//variables de forma global
+//process: es un objeto global dentro de node, y es actualizado dependiendo del entorno donde esta corriendo
+
+//PUERTO
+process.env.PORT = process.env.PORT || 8080;
